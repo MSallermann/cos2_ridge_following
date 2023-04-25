088059d03eb7962d9ee8cacd7e937702e44130b4
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-04-25 17:05:01 +0200

    RidgeFolower: did some implementation work

--- src/ridgefollowing/algorithms/ridgefollower.py
+++ src/ridgefollowing/algorithms/ridgefollower.py
@@ -1,7 +1,7 @@
 from ridgefollowing import energy_surface
 from ridgefollowing.algorithms import modes
 import numpy.typing as npt
-from typing import Optional
+from typing import Optional, List
 import numpy as np
 from scipy.optimize import minimize
 import numdifftools as nd
@@ -13,10 +13,16 @@ class RidgeFollower:
         energy_surface: energy_surface.EnergySurface,
         maxiter: int = 1000,
         tolerance: Optional[float] = None,
+        n_iterations_folow: int = 100,
+        radius: float = 0.5e-2,
     ) -> None:
-        self.esurf = energy_surface
+        self.esurf: energy_surface.EnergySurface = energy_surface
         self.tolerance: float = tolerance
-        self.maxiter = maxiter
+        self.maxiter: int = maxiter
+        self.radius: float = radius
+        self.n_iterations_follow: int = n_iterations_folow
+
+        self.history: List[npt.NDArray] = []
 
     def C(self, x: npt.ArrayLike) -> float:
         """Computes the cos function
@@ -54,9 +60,7 @@ class RidgeFollower:
         """
         return self.fd_grad_C(x)
 
-    def find_maximum_on_ring(
-        self, x0: npt.NDArray, d0: npt.NDArray, radius: float = 1.0
-    ):
+    def find_maximum_on_ring(self, x0: npt.NDArray, d0: npt.NDArray):
         """Finds the maximum value of C2 on a ring with radius around x0. d0 is the initial guess for the direction of the maximum
 
         Args:
@@ -65,18 +69,15 @@ class RidgeFollower:
             radius (float, optional): radius of ring. Defaults to 1.
         """
         d0 /= np.linalg.norm(d0)
-        d = np.array(d0)
-
-        run = True
 
         def fun(d):
-            """C(x0 + radius*d)**2"""
-            return -self.C(x0 + radius * d) ** 2
+            """-C(x0 + radius*d)**2. Minus us sign because we use scipy.minimize"""
+            return -self.C(x0 + self.radius * d) ** 2
 
         def grad(d):
-            """gradient of C(x0 + radius * d) wrt to d"""
-            x = x0 + radius * d
-            grad_c2_d = 2 * self.C(x) * self.grad_C(x) * radius
+            """gradient of C(x0 + radius * d) wrt to d. Minus us sign because we use scipy.minimize"""
+            x = x0 + self.radius * d
+            grad_c2_d = 2 * self.C(x) * self.grad_C(x) * self.radius
             # project out component along d0
             grad_c2_d -= np.dot(grad_c2_d, d) * d
             return -grad_c2_d
@@ -95,4 +96,18 @@ class RidgeFollower:
             callback=cb,
         )
 
-        return res.x
+        return [res.x, -res.fun, -res.jac]
+
+    def follow(self, x0: npt.NDArray, d0: npt.NDArray):
+        self.history = []
+
+        x_cur = np.array(x0)
+        d_cur = np.array(d0)
+
+        for i in range(self.n_iterations_follow):
+            # Find maximum on ring
+            d_cur, c2, grad_c2 = self.find_maximum_on_ring(x_cur, d_cur)
+            x_cur += self.radius * d_cur
+
+            E = self.esurf.energy(x_cur)
+            self.history.append(np.array([*x_cur, *d_cur, c2, *grad_c2, E]))
