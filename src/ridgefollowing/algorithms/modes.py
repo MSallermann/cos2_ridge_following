5ed20084c2796c4c93f95a5cfd900ed44f399fca
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-04-27 20:12:34 +0200

    modes: implemented lowest_n_modes

--- src/ridgefollowing/algorithms/modes.py
+++ src/ridgefollowing/algorithms/modes.py
@@ -15,6 +15,19 @@ def lowest_mode(matrix: npt.ArrayLike) -> npt.NDArray:
     return evals[0], evecs[:, 0]
 
 
+def lowest_n_modes(matrix: npt.ArrayLike, n: int):
+    """Computes the lowest n eigenvalues and modes
+
+    Args:
+        matrix (npt.ArrayLike): the matrix for which to compute the lowest mode
+
+    Returns:
+        npt.NDArray float, npt.NDArray: Tupel of lowest (eval, evec)
+    """
+    evals, evecs = np.linalg.eigh(matrix)
+    return evals[:n], evecs[:, :n]
+
+
 def eigenpairs(matrix: npt.ArrayLike) -> npt.NDArray:
     """Returns eigenvalues and vectors, sorted by eigenvalue
 
