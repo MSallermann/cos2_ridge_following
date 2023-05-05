b391c35b04e3daa44d92b8d0a4af804fb48d7b8e
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-05-05 13:41:26 +0200

    generalized cubic surface

--- src/ridgefollowing/surfaces/cubic.py
+++ src/ridgefollowing/surfaces/cubic.py
@@ -5,21 +5,43 @@ import numpy.typing as npt
 
 class CubicSurface(quadratic.QuadraticSurface):
     def __init__(self, matrix=np.diag([1.0, 2.0])):
-        self.f = 0.1
+        self.f1 = 0.1
+        self.f2 = 0.1
+        self.c1 = -0.3
+        self.c2 = 0.2
         super().__init__(matrix, 2)
 
     def energy(self, x: npt.ArrayLike) -> float:
-        return super().energy(x) + self.f * x[0] ** 2 * x[1]
-        # return 0.5 * np.transpose(x) @ self._hessian @ x
+        return (
+            super().energy(x)
+            + self.f1 * x[0] ** 2 * x[1]
+            + self.f2 * x[1] ** 2 * x[0]
+            + self.c1 * x[0] ** 3
+            + self.c2 * x[1] ** 3
+        )
 
     def gradient(self, x: npt.ArrayLike) -> npt.NDArray:
         g = super().gradient(x)
-        g += np.array([2 * self.f * x[0] * x[1], self.f * x[0] ** 2])
+        g += self.f1 * np.array([2.0 * x[0] * x[1], x[0] ** 2])
+
+        g += self.f2 * np.array(
+            [
+                x[1] ** 2,
+                2.0 * x[1] * x[0],
+            ]
+        )
+
+        g[0] += 3 * self.c1 * x[0] ** 2
+        g[1] += 3 * self.c2 * x[1] ** 2
         return g
 
     def hessian(self, x: npt.ArrayLike) -> npt.NDArray:
         h = super().hessian(x)
-        h += np.array(
-            [[2.0 * self.f * x[1], 2.0 * self.f * x[0]], [2.0 * self.f * x[0], 0.0]]
-        )
+
+        h += self.f1 * np.array([[2.0 * x[1], 2.0 * x[0]], [2.0 * x[0], 0.0]])
+
+        h += self.f2 * np.array([[2.0 * x[1], 0.0], [2.0 * x[0], 2.0 * x[1]]])
+
+        h[0, :] += 6.0 * self.c1 * x[0]
+        h[1, :] += 6.0 * self.c2 * x[1]
         return h
