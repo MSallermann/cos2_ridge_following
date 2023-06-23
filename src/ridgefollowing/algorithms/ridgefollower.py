602648ff25117b717d3485ca6e251af39252f37e
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-06-23 15:22:31 +0000

    ridge_follower: more functionality

--- src/ridgefollowing/algorithms/ridgefollower.py
+++ src/ridgefollowing/algorithms/ridgefollower.py
@@ -11,11 +11,12 @@ class RidgeFollower(abc.ABC):
         self,
         energy_surface: energy_surface.EnergySurface,
         n_iterations_follow: int = 100,
+        output_path: Optional[Path] = None,
     ) -> None:
         self.esurf: energy_surface.EnergySurface = energy_surface
         self.n_iterations_follow: int = n_iterations_follow
         self.print_progress: bool = True
-        self.output_path: Path = Path(".")
+        self.output_path: Optional[Path] = output_path
 
         self._iteration: Optional[int] = None
         self._x_cur: npt.NDArray = np.zeros(self.esurf.ndim)
@@ -24,6 +25,8 @@ class RidgeFollower(abc.ABC):
         self._step_cur: npt.NDArray = np.zeros(self.esurf.ndim)
         self._G: npt.NDArray = np.zeros(self.esurf.ndim)
         self._E: float = 0.0
+
+        self.__stop = False
 
     def setup_history(self):
         self.history: dict = dict(
@@ -41,20 +44,49 @@ class RidgeFollower(abc.ABC):
         self.history["G"][self._iteration] = self._G
         self.history["G_norm"][self._iteration] = np.linalg.norm(self._G)
 
-    def dump_history(self, folder: Optional[Path] = None):
-        if folder is None:
-            folder = self.output_path
+    def dump_history(self):
+        if self.output_path is None:
+            raise Exception("Output path needs to be set")
 
+        folder = self.output_path / "history"
         folder.mkdir(exist_ok=True)
+
         for k, v in self.history.items():
             np.save(folder / k, v)
+
+    def plot_history(self):
+        import matplotlib.pyplot as plt
+
+        if self.output_path is None:
+            raise Exception("Output path needs to be set")
+
+        folder = self.output_path / "plot_history"
+        folder.mkdir(exist_ok=True)
+
+        for k, v in self.history.items():
+            if len(v.shape) == 1:
+                plt.plot(v, marker=".")
+                plt.xlabel("iteration")
+                plt.ylabel(k)
+                plt.savefig(str(folder / f"{k}.png"))
+                plt.tight_layout()
+                plt.close()
+
+    def stop(self):
+        """Call this in the determine_step function to gracefully stop the run"""
+        self.__stop = True
+        for k in self.history.keys():
+            self.history[k] = self.history[k][: self._iteration + 1]
 
     @abc.abstractmethod
     def determine_step(self):
         ...
 
     def follow(self, x0: npt.NDArray, d0: npt.NDArray):
+        d0 /= np.linalg.norm(d0)
+
         self.setup_history()
+        self.__stop = False
 
         self._x_cur = np.array(x0)
         self._step_cur = d0
@@ -78,3 +110,6 @@ class RidgeFollower(abc.ABC):
             self.log_history()
 
             self._x_cur += self._step_cur
+
+            if self.__stop:
+                break
