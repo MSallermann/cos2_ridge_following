42bd5971f81eb6636f4c1c98021fa9e545749e7b
Moritz Sallermann <m.sallermann@fz-juelich.de>
2023-07-05 14:55:24 +0000

    jitted cubic and quadratic surface

--- src/ridgefollowing/surfaces/cubic.py
+++ src/ridgefollowing/surfaces/cubic.py
@@ -1,66 +1,39 @@
 from ridgefollowing.surfaces import quadratic
 import numpy as np
 import numpy.typing as npt
+from numba.experimental import jitclass
+from numba import float64, int32
 
 
-class CubicSurface(quadratic.QuadraticSurface):
-    def __init__(self, hessian=np.diag([0.0, 0.0]), gradient=np.zeros(2)):
+@jitclass(
+    [
+        ("x2y", float64),
+        ("y2x", float64),
+        ("x3", float64),
+        ("y3", float64),
+        ("factor", float64),
+        ("ndim", int32),
+    ]
+)
+class CubicSurfaceHelper:
+    def __init__(self, x2y, y2x, x3, y3, factor, ndim):
         self.x2y = -0.1  # x**2 y
         self.y2x = -0.3  # y**2 x
         self.x3 = 0.15  # x**3
         self.y3 = -0.5  # y**3
         self.factor = 1.0
-        super().__init__(hessian=hessian, gradient=gradient, ndim=2)
-
-    def setup_quapp_example(self, example_idx: int):
-        """Sets up the parameters to reproduce the examples from the article
-        'Gradient extremals and valley floor bifurcations on potential energy surfaces' by Quapp et al. (1988)
-        """
-
-        if example_idx == 2:
-            # 1/2 * (xy + 2)*(y-x)
-            self._hessian = np.zeros(shape=(2, 2))
-            self._g0 = np.array([-1, 1])
-            self.x2y = -0.5
-            self.y2x = 0.5
-            self.x3 = 0.0
-            self.y3 = 0.0
-        elif example_idx == 3:
-            # 1/2 * (xy - 2)*(y-x)
-            self._hessian = np.zeros(shape=(2, 2))
-            self._g0 = np.array([1, -1])
-            self.x2y = -0.5
-            self.y2x = 0.5
-            self.x3 = 0.0
-            self.y3 = 0.0
-        elif example_idx == 4:
-            # 0.5 * (xy**2 - yx**2 + mu * x**2 + 2y - 3)
-            self.mu = 1.0
-            self._hessian = self.mu * np.array(
-                [[1.0, 0.0], [0.0, 0.0]]
-            )  # Notice: the quadratic energy contains a factor of 0.5 already
-            self.y2x = 0.5
-            self.x2y = -0.5
-            self.x3 = 0.0
-            self.y3 = 0.0
-            self._g0 = np.array([0.0, 1.0])
-        else:
-            raise ValueError("Invalid example index")
-
-    def scale_anharmonicity(self, factor):
-        self.factor = factor
+        self.ndim = ndim
 
     def energy(self, x: npt.ArrayLike) -> float:
         return (
-            super().energy(x)
-            + self.factor * self.x2y * x[0] ** 2 * x[1]
+            self.factor * self.x2y * x[0] ** 2 * x[1]
             + self.factor * self.y2x * x[1] ** 2 * x[0]
             + self.factor * self.x3 * x[0] ** 3
             + self.factor * self.y3 * x[1] ** 3
         )
 
     def gradient(self, x: npt.ArrayLike) -> npt.NDArray:
-        g = super().gradient(x)
+        g = np.zeros(self.ndim)
         g += self.factor * self.x2y * np.array([2.0 * x[0] * x[1], x[0] ** 2])
         g += (
             self.factor
@@ -77,7 +50,7 @@ class CubicSurface(quadratic.QuadraticSurface):
         return g
 
     def hessian(self, x: npt.ArrayLike) -> npt.NDArray:
-        h = quadratic.QuadraticSurface.hessian(self, x)
+        h = np.zeros((self.ndim, self.ndim))
         h += (
             self.factor
             * self.x2y
@@ -91,3 +64,62 @@ class CubicSurface(quadratic.QuadraticSurface):
         h[0, 0] += 6.0 * self.factor * self.x3 * x[0]
         h[1, 1] += 6.0 * self.factor * self.y3 * x[1]
         return h
+
+
+class CubicSurface(quadratic.QuadraticSurface):
+    """A twodimensional cubic surface"""
+
+    def __init__(self, hessian=np.diag([0.0, 0.0]), gradient=np.zeros(2)):
+        self.params = CubicSurfaceHelper(
+            x2y=-0.1, y2x=-0.3, x3=0.1, y3=-0.5, factor=1.0, ndim=2
+        )
+
+        self.surf_quad = quadratic.QuadraticSurface(hessian=hessian, gradient=gradient)
+
+        super().__init__(hessian=hessian, gradient=gradient, ndim=2)
+
+    def setup_quapp_example(self, example_idx: int):
+        """Sets up the parameters to reproduce the examples from the article
+        'Gradient extremals and valley floor bifurcations on potential energy surfaces' by Quapp et al. (1988)
+        """
+        if example_idx == 2:
+            # 1/2 * (xy + 2)*(y-x)
+            self.surf_quad.params._hessian = np.zeros(shape=(2, 2))
+            self.surf_quad.params.g0 = np.array([-1, 1])
+            self.params.x2y = -0.5
+            self.params.y2x = 0.5
+            self.params.x3 = 0.0
+            self.params.y3 = 0.0
+        elif example_idx == 3:
+            # 1/2 * (xy - 2)*(y-x)
+            self.surf_quad.params._hessian = np.zeros(shape=(2, 2))
+            self.surf_quad.params.g0 = np.array([1, -1])
+            self.params.x2y = -0.5
+            self.params.y2x = 0.5
+            self.params.x3 = 0.0
+            self.params.y3 = 0.0
+        elif example_idx == 4:
+            # 0.5 * (xy**2 - yx**2 + mu * x**2 + 2y - 3)
+            self.surf_quad.params._hessian = self.params.mu * np.array(
+                [[1.0, 0.0], [0.0, 0.0]]
+            )  # Notice: the quadratic energy contains a factor of 0.5 already
+            self.surf_quad.params.g0 = np.array([0.0, 1.0])
+            self.params.mu = 1.0
+            self.params.y2x = 0.5
+            self.params.x2y = -0.5
+            self.params.x3 = 0.0
+            self.params.y3 = 0.0
+        else:
+            raise ValueError("Invalid example index")
+
+    def energy(self, x: npt.NDArray) -> float:
+        return self.surf_quad.energy(x) + self.params.energy(x)
+
+    def gradient(self, x: npt.NDArray) -> npt.NDArray:
+        return self.surf_quad.gradient(x) + self.params.gradient(x)
+
+    def hessian(self, x: npt.NDArray) -> npt.NDArray:
+        return self.surf_quad.hessian(x) + self.params.hessian(x)
+
+    def scale_anharmonicity(self, factor):
+        self.params.factor = factor
--- src/ridgefollowing/surfaces/quadratic.py
+++ src/ridgefollowing/surfaces/quadratic.py
@@ -2,6 +2,29 @@ from ridgefollowing import energy_surface
 import numpy as np
 import numpy.typing as npt
 from typing import Optional
+from numba.experimental import jitclass
+from numba import float64, int32
+
+
+@jitclass(
+    [
+        ("g0", float64[::1]),
+        ("_hessian", float64[:, ::1]),
+    ]
+)
+class QuadraticSurfaceHelper:
+    def __init__(self, hessian: npt.NDArray, g0: npt.NDArray):
+        self.g0 = g0
+        self._hessian = hessian
+
+    def energy(self, x: npt.NDArray) -> float:
+        return self.g0 @ x + 0.5 * x @ self._hessian @ x
+
+    def gradient(self, x: npt.NDArray) -> npt.NDArray:
+        return self.g0 + self._hessian @ x
+
+    def hessian(self, x: npt.NDArray) -> npt.NDArray:
+        return self._hessian.copy()
 
 
 class QuadraticSurface(energy_surface.EnergySurface):
@@ -12,17 +35,20 @@ class QuadraticSurface(energy_surface.EnergySurface):
         ndim: int = 2,
     ):
         super().__init__(ndim)
+
         self._hessian = hessian
         if gradient is None:
-            self._g0 = np.zeros(ndim)
+            g0 = np.zeros(ndim, dtype=float)
         else:
-            self._g0 = gradient
+            g0 = gradient
+
+        self.params = QuadraticSurfaceHelper(hessian=hessian, g0=g0)
 
     def energy(self, x: npt.ArrayLike) -> float:
-        return np.dot(self._g0, x) + 0.5 * np.transpose(x) @ self._hessian @ x
+        return self.params.energy(x)
 
     def gradient(self, x: npt.ArrayLike) -> npt.NDArray:
-        return self._g0 + self._hessian @ x
+        return self.params.gradient(x)
 
     def hessian(self, x: npt.ArrayLike) -> npt.NDArray:
-        return self._hessian.copy()
+        return self.params.hessian(x)
